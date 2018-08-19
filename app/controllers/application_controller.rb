0011e0b51f99903bd05421917d81b16960c3820b
Alejandro Sabogal <asabogal.git@gmail.com>
2018-08-19 23:44:28 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -33,6 +33,8 @@ class ApplicationController < Sinatra::Base
 		@user = User.find_by(username: params[:username])
 		if @user
 			redirect "/success"
+		else
+			redirect "/failure"
 	end
 
 	get "/success" do
